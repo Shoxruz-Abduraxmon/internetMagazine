346099b4528b1bf9cb1dc1c0d90ba5207194ddf6
Shoxruz-Abduraxman <ab.shoxruz@gmail.com>
2023-07-14 19:15:14 +0500

    utf-8

--- data/baza.js
+++ data/baza.js
@@ -1,12 +1,29 @@
+const uuid = require('uuid');
+const fs = require('fs');
+const path = require('path');
+
+
 class Components {
     constructor(img, name, vazifasi, maosh) {
         this.img = img,
         this.name = name, 
         this.vazifasi = vazifasi,
-        this.maosh = maosh
+        this.maosh = maosh,
+        this.id = id()
     }
 
     save() {
-        
+
     }
+
+    static getAll() {
+        fs.readFile(
+            path.join(__dirname, '..', 'malumotlarBazasi', 'sklat.json'),
+            'utf-8',
+            (err, content) => {
+                if(err) throw err
+                
+            }
+        )
+    } 
 }
