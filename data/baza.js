bf66aa02b47fc0716a224b6d33ea568458d4781b
Shoxruz-Abduraxman <ab.shoxruz@gmail.com>
2023-07-15 16:56:04 +0500

    va nihoyat to`g`ri ishladi

--- data/baza.js
+++ data/baza.js
@@ -1,29 +1,40 @@
-const uuid = require('uuid');
+// const uuid = require('uuid');
 const fs = require('fs');
 const path = require('path');
 
 
 class Components {
     constructor(img, name, vazifasi, maosh) {
-        this.img = img,
-        this.name = name, 
-        this.vazifasi = vazifasi,
-        this.maosh = maosh,
-        this.id = id()
+        this.img = img
+        this.name = name 
+        this.vazifasi = vazifasi
+        this.maosh = maosh
+        this.id = null
     }
 
-    save() {
-
+    async save() {
+        let kampaniya = await Components.getAll()
+        console.log('kanpaniya', kampaniya);
     }
 
     static getAll() {
-        fs.readFile(
-            path.join(__dirname, '..', 'malumotlarBazasi', 'sklat.json'),
-            'utf-8',
-            (err, content) => {
-                if(err) throw err
-                
-            }
-        )
+        return new Promise((resolve, reject) => {
+            fs.readFile(
+                path.join(__dirname, '..', 'malumotlarBazasi', 'sklat.json'),
+                'utf-8',
+                (err, content) => {
+                    if(err) {
+                        resolve (err) 
+                    } else {
+                        resolve(JSON.parse(content))
+                    }
+                    
+                }
+            )
+        })
+
+        
     } 
 }
+
+module.exports = Components;
